d576128e292bb24216394234248af59be261f6fb
Randall Lunetto <rlunetto@gmail.com>
2020-11-01 21:12:18 -0300

    Update preparation.sh

--- preparation.sh
+++ preparation.sh
@@ -16,14 +16,4 @@ cd $ADIR/exercises/pig_etl
 # Capture the first 25 records to test script
 head -n 25 $ADIR/data/ad_data1.txt > sample1.txt
 
-# Open Grunt shell in local mode to work with the local sample1.txt file locally in Pig as test environment.
-pig -x local
-
-# Load data in the sample1.txt file into Pig and dump.
-data = LOAD 'sample1.txt';
-DUMP data;
-
-# Load the first two columns’ data from the sample file as character data and dump that data.
-first_2_columns = LOAD 'sample1.txt' AS (keyword:chararray, campaign_id:chararray);
-DUMP first_2_columns;
-
+# Open GRUNT Shell to work in Pig Latin...
