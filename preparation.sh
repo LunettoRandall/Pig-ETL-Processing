7c34a142952a588cd475eec39c9bae33bf7a3bd5
Randall Lunetto <rlunetto@gmail.com>
2020-11-01 21:09:15 -0300

    Update preparation.sh

--- preparation.sh
+++ preparation.sh
@@ -13,4 +13,12 @@ export ADIR=/home/rlunett/labPigETL
 # Change to the directory for the pig code and list the pig file
 cd $ADIR/exercises/pig_etl
 
+# Capture the first 25 records to test script
+head -n 25 $ADIR/data/ad_data1.txt > sample1.txt
 
+# Open Grunt shell in local mode to work with the local sample1.txt file locally in Pig as test environment.
+pig -x local
+
+# Load data in the sample1.txt file into Pig and dump.
+data = LOAD 'sample1.txt';
+DUMP data;
