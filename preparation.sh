76cdd21a545f22eaded4a46f188bc9308b07c0a5
Randall Lunetto <rlunetto@gmail.com>
2020-11-01 21:10:21 -0300

    Update preparation.sh

--- preparation.sh
+++ preparation.sh
@@ -22,3 +22,8 @@ pig -x local
 # Load data in the sample1.txt file into Pig and dump.
 data = LOAD 'sample1.txt';
 DUMP data;
+
+# Load the first two columns’ data from the sample file as character data and dump that data.
+first_2_columns = LOAD 'sample1.txt' AS (keyword:chararray, campaign_id:chararray);
+DUMP first_2_columns;
+
