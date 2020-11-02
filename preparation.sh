74ea776537c3ae75fd4af12224ab892f8ad0258b
Randall Lunetto <rlunetto@gmail.com>
2020-11-01 21:05:15 -0300

    Update preparation.sh

--- preparation.sh
+++ preparation.sh
@@ -7,4 +7,10 @@ tar zxvf labPigETL.tgz
 # Remove the tgz file to save storage on your server.
 rm labPigETL.tgz
 
+# Export command is to set the directory path for the variable ADIR
+export ADIR=/home/rlunett/labPigETL
+
+# Change to the directory for the pig code and list the pig file
+cd $ADIR/exercises/pig_etl
+
 
