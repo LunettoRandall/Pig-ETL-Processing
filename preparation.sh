f9c4e851e306ba1e50fc40911003d6124d0ce856
Randall Lunetto <rlunetto@gmail.com>
2020-11-01 21:01:31 -0300

    Create preparation.sh

---
+++ preparation.sh
@@ -0,0 +1,10 @@
+# Download Dataset
+wget https://s3.amazonaws.com/hipicdatasets/labPigETL.tgz
+
+# Unzip file
+tar zxvf labPigETL.tgz
+
+# Remove the tgz file to save storage on your server.
+rm labPigETL.tgz
+
+
